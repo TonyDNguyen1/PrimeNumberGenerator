101286437c332961dcc08418135c22d2da53c0f6
Tiffany Vuong <Tiffany Vuong>
2016-06-14 00:00:16 -0700

    First completed version using a simple prime number generator

--- CSharpPrimeGenerator/CSharpPrimeGenerator/Program.cs
+++ CSharpPrimeGenerator/CSharpPrimeGenerator/Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
+using System.Diagnostics;
 
 namespace CSharpPrimeGenerator
 {
@@ -11,17 +14,72 @@ namespace CSharpPrimeGenerator
         static void Main(string[] args)
         {
             var simplePrimeGenerator = new SimplePrimeGenerator();
-            var taskSimplePrimeGenerator = simplePrimeGenerator.GeneratePrimeAsync(100000000);
-            Task.WhenAll(taskSimplePrimeGenerator);
-            var primeList = taskSimplePrimeGenerator.Result;
 
-            //List<uint> primeList = simplePrimeGenerator.GeneratePrimeAsync(1000000000).GetAwaiter().GetResult();
-            /*foreach(var prime in primeList)
+            //Create a cancellation token for stopping the prime number generation task.
+            var tokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = tokenSource.Token;
+            var tasks = new ConcurrentBag<Task>();
+
+            //List<T> has a max capacity at uint.MaxValue/2.  Pass it as the limit to be calculated if there is no time limit.
+            var limit = uint.MaxValue / 2;
+            var task = Task.Factory.StartNew(() => simplePrimeGenerator.GeneratePrimeAsync(limit,cancellationToken), cancellationToken);
+            tasks.Add(task);
+
+            //Get the process/program start time.
+            //The start time can be a couple of seconds if this is a cold start.
+            //The JIL can take a few seconds to create the native code on the first run.
+            //Run this test twice to get a better results, or use NGen to precompile the .NET assemblies to native code.
+            //Use RyuJIT instead of JIT.  RyuJIT is faster than JIT. https://gist.github.com/richlander/d29dc4fe4e8032de5c92
+
+            //This loop will output a counter number and a max prime number for every second.
+            //Do not output too many statements as these can affect the memory allocations and the CPU.
+            var startTime = DateTime.Now; //Process.GetCurrentProcess().StartTime;
+            TimeSpan duration = DateTime.Now-startTime;
+            var secondsRan = Math.Round(duration.TotalMilliseconds / 1000d);
+            while (secondsRan < 60d) //exit the loop if the program has ran for more than 60 seconds.
+            {                
+                var nextStop = secondsRan + 1d;
+                int waitTime = (int)((nextStop* 1000d) - duration.TotalMilliseconds);
+                Thread.Sleep(waitTime);
+                System.Console.WriteLine($"Time(sec): {((int)nextStop).ToString("D2")} ----- Max Prime #: {simplePrimeGenerator.GetMaxPrime().ToString("N0")}");
+                duration = DateTime.Now - startTime;
+                secondsRan = Math.Round(duration.TotalMilliseconds / 1000d);
+            }
+
+            //Stop generating the prime numbers
+            tokenSource.Cancel();
+
+            try
             {
-                System.Console.WriteLine(prime);
-            }*/
-            System.Console.WriteLine(primeList.Count());
-            System.Console.Read();
+                Task.WhenAll(tasks.ToArray());
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine("\nAggregateException thrown with the following inner exceptions:");
+                // Display information about each exception. 
+                foreach (var v in e.InnerExceptions)
+                {
+                    if (v is TaskCanceledException)
+                        Console.WriteLine("   TaskCanceledException: Task {0}",
+                                          ((TaskCanceledException)v).Task.Id);
+                    else
+                        Console.WriteLine("   Exception: {0}", v.GetType().Name);
+                }
+                Console.WriteLine();
+            }
+            finally
+            {
+                tokenSource.Dispose();
+            }
+
+            if (task != null)
+            {
+                var primeList = task.Unwrap().Result;
+                //Please note, some additional prime numbers are calculated after 60 seconds before the task was cancelled.
+                System.Console.WriteLine($"Number of primes: {primeList.Count().ToString("N0")}");
+            }
+            System.Console.WriteLine("Hit a key to exit.");
+            System.Console.ReadKey(true);
         }
     }
 }
--- CSharpPrimeGenerator/CSharpPrimeGenerator/SimplePrimeGenerator.cs
+++ CSharpPrimeGenerator/CSharpPrimeGenerator/SimplePrimeGenerator.cs
@@ -2,19 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CSharpPrimeGenerator
 {
     public class SimplePrimeGenerator
     {
-        public async Task<List<uint>> GeneratePrimeAsync(int max)
+        //Use the running flag to prevent GeneratePrimeAsync from running concurrent tasks.
+        private bool running = false;
+
+        //This has the max prime number generated from GeneratePrimeAsync.
+        //A lock will not be put on this on assigning it.  Assigning a uint value is an atomic action.
+        //A lock would enforce the order of the read and write but this is not important.  Performance is more important.
+        private uint maxPrime = 2;
+
+        public uint GetMaxPrime()
         {
-            var result = await Task.Run(() =>
+            return maxPrime;
+        }
+
+        public async Task<List<uint>> GeneratePrimeAsync(uint max, CancellationToken ct)
+        {
+            lock (this)
             {
+                if (running)
+                {
+                    //Exit if another task is running
+                    return null;
+                }
+                else
+                {
+                    //Set running to true so that another concurrent process is prevented from running.
+                    running = true;
+                }
+            }
+
+            List<uint> results = null;
+
+            if (ct.IsCancellationRequested == true)
+            {
+                ct.ThrowIfCancellationRequested();
+                return null;
+            }
+
+            results = await Task.Run(() =>
+            {
+                //Exit early if the max is less than 5.
                 if (max < 2)
                 {
-                    return new List<uint>(); //return an empty list
+                    return null;
                 }
                 if (max == 2)
                 {
@@ -25,33 +62,69 @@ namespace CSharpPrimeGenerator
                     return new List<uint>() { 2, 3 };
                 }
 
-                List<uint> primeList = new List<uint>() { 2, 3, 5 };
-                for (uint i = 7; i <= max; i++)
-                {
-                    uint sqrtPrime = (uint)Math.Sqrt((double)i);
+                //"2" is not included in the initial prime list because the event numbers are skipped.
+                //Use List<uint> instead of uint[] for the collection because List<unit> doubles its capacity automatically.
+                //There is an overhead on copying the old list into the new list when the capacity is increased.
+                List<uint> primeList = new List<uint>() { 3, 5 };
 
-                    foreach (uint prime in primeList)
+                try
+                {
+                    //A number 'i' is prime if it is not divisible by every prime number less than or equal to the square root of 'i'.
+                    //Skip even numbers.
+                    for (uint i = 7; i <= max; i = i + 2)
                     {
-                        if (prime <= sqrtPrime)
+                        uint sqrtPrime = (uint)Math.Sqrt((double)i);
+
+                        foreach (uint prime in primeList)
                         {
-                            if (i % prime == 0)
+                            if (ct.IsCancellationRequested)
                             {
-                                //i is not a prime number
+                                //exit both the outer and inner loops
+                                i = max + 1;
+                                break;
+                            }
+
+                            //test divisibility for only the prime numbers that are less than the square root of 'i'
+                            if (prime <= sqrtPrime)
+                            {
+                                //'i' is divisible by a prime if 'i' modulo prime is 0 (i/prime and its remainder is 0)
+                                if (i % prime == 0)
+                                {
+                                    //'i' is not a prime number; exit the inner loop and test the next 'i'.
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                //Since 'i' is not divisible by the primes, it is a prime number.
+                                maxPrime = i;
+                                primeList.Add(i);
                                 break;
                             }
                         }
-                        else
-                        {
-                            primeList.Add(i);
-                            break;
-                        }
                     }
                 }
+                catch (OperationCanceledException)
+                {
+                    System.Console.WriteLine("Time is up!  Stop calculating prime numbers.");
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine($"There is an error: {ex.Message}");
+                }
+
+                //Insert 2 as the first prime number
+                primeList.Insert(0, 2);
                 return primeList;
+            }, ct);
 
-            });
+            //Toggle the running flag so that this async method can be run again.
+            lock (this)
+            {
+                running = false;
+            }
 
-            return result;
+            return results;
         }
     }
 }
